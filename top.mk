fa7f0d8475b9346166582299457bf7b2c296ac04
Michael Builov <mbuilov@gmail.com>
2017-01-27 11:05:36 +0300

    add reference to clean-build project page in error message

--- top.mk
+++ top.mk
@@ -3,5 +3,6 @@ TOP := $(abspath $(dir $(lastword $(MAKEFILE_LIST))))
 
 # clean-build path
 ifndef MTOP
-$(error MTOP - path to clean-build is not defined, example: MTOP=/usr/local/clean-build or MTOP=C:\User\clean-build)
+$(error MTOP - path to clean-build (https://github.com/mbuilov/clean-build) is not defined,\
+ example: MTOP=/usr/local/clean-build or MTOP=C:\User\clean-build)
 endif
